3d126dbee9f6be2cf856b8f45b52a1d885a832f2
Ayyoub Maknassa <ayyoub.maknassa@hotmail.com>
2024-08-06 11:17:22 +0100

    [CI] Add GitHub Actions workflow for deploying to Heroku

--- basicapi/settings.py
+++ basicapi/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-9da6zyr1-otay1m@2fqmz!g(bok2$l(tu&opq#g1ihi+$pw2q$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
